2b42ad8604495415dcbf6b8e5a29e000efd7ad9c
Gian <Giansimbolon21@gmail.com>
2026-04-18 14:01:10 +0800

    Initial Commit

---
+++ CH2-Mail/CH2_MailApp.swift
@@ -0,0 +1,17 @@
+//
+//  CH2_MailApp.swift
+//  CH2-Mail
+//
+//  Created by Gian Denggan Benjamin on 18/04/26.
+//
+
+import SwiftUI
+
+@main
+struct CH2_MailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CH2-Mail/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CH2-Mail
+//
+//  Created by Gian Denggan Benjamin on 18/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
